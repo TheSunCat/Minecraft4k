8614252e082663a22d69d00b336d7495072d686a
Matias <mat@allpurposem.at>
2025-01-22 19:02:04 +0100

    Constants.h should not contain any doubles!

--- Constants.h
+++ Constants.h
@@ -42,13 +42,13 @@
 
 // from https://iquilezles.org/articles/float4k/
 // better compressible floats
-#define p0d003 0.00299072265625 // 0.003    0x3b440000
-#define p0d02 0.0200195313f     // 0.02f    0x3ca40000
-#define p0d10 0.1000976563f     // 0.10f    0x3dcd0000
-#define p0d30 0.3007812500f     // 0.30f    0x3e9a0000
-#define p0d60 0.6015625000f     // 0.60f    0x3f1a0000
-#define p0d65 0.6484375000f     // 0.65f    0x3f260000
-#define p0d80 0.8007812500f     // 0.80f    0x3f4d0000
-#define p0d99 0.9882812500f     // 0.99f    0x3f7d0000
+#define p0d003 0.00299072265625f  // 0.003    0x3b440000
+#define p0d02 0.0200195313f       // 0.02f    0x3ca40000
+#define p0d10 0.1000976563f       // 0.10f    0x3dcd0000
+#define p0d30 0.3007812500f       // 0.30f    0x3e9a0000
+#define p0d60 0.6015625000f       // 0.60f    0x3f1a0000
+#define p0d65 0.6484375000f       // 0.65f    0x3f260000
+#define p0d80 0.8007812500f       // 0.80f    0x3f4d0000
+#define p0d99 0.9882812500f       // 0.99f    0x3f7d0000
 
-#define HALF_PI 1.5703125      // 1.5703125 0x3fc90000
+#define HALF_PI 1.5703125f  // 1.5703125 0x3fc90000
